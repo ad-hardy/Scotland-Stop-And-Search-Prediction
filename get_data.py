f5d597737f60bd38d3185e0d15dd25656b1ec2e2
Adam Hardy <adh167@gmail.com>
2021-04-16 17:21:19 +0100

    added get_data function

--- get_data.py
+++ get_data.py
@@ -4,7 +4,13 @@ import json
 import urllib.request as urllib
 import zipfile
 import os
+import time
 
+def get_data(folder="data"):
+    df_stop_search = get_stop_search()
+    df_simd = get_simd()
+    df_wards = get_zones_wards(df_simd)
+    return df_stop_search, df_simd, df_wards
 
 def get_stop_search(folder="data"):
     url = "https://www.scotland.police.uk/spa-media/pssjskv1/1b-01-april-31-december-2020-csv.csv"
@@ -33,16 +39,18 @@ def get_simd(folder="data"):
 def geog_url(code):
     return f"https://statistics.gov.scot/resource.json?uri=http%3A%2F%2Fstatistics.gov.scot%2Fid%2Fstatistical-geography%2F{code}"
 
-def get_zones_wards(df_simd, folder="data"):
+def get_zones_wards(df_simd, folder="data", rate_limit=1/15):
     df_wards = pd.DataFrame(columns = ['zone_code' , 'zone_name', 'ward_code' , 'ward_name'])
     i = 0
     for zone_code in df_simd["Data_Zone"]:
+        time.sleep(rate_limit)
         r = requests.get(geog_url(zone_code))
         zone_data = json.loads(r.text)[0]
         zone_name = zone_data["http://www.w3.org/2000/01/rdf-schema#label"][0]["@value"]
         ward_code = zone_data["http://statistics.gov.scot/def/hierarchy/best-fit#electoral-ward"][0]["@id"].split("/")[-1]
         
         #get the name of the ward
+        time.sleep(rate_limit)
         r = requests.get(geog_url(ward_code))
         json.loads(r.text)[0]
         
@@ -58,11 +66,5 @@ def get_zones_wards(df_simd, folder="data"):
 
 if __name__ == "__main__":
 
-    df_stop_search = get_stop_search()
-    print(df_stop_search)
+    df_stop_search, df_simd, df_wards = get_data()
 
-    df_simd = get_simd()
-    print(df_simd)
-
-    df_wards = get_zones_wards(df_simd)
-    print(df_wards)
