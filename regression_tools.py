c2735f490685f6ec8219cad1057f93fa0d647c5e
Adam Hardy <adh167@gmail.com>
2021-04-20 14:25:37 +0100

    base model

---
+++ regression_tools.py
@@ -0,0 +1,17 @@
+def drop_multi_cols(dfs:list, cols:list):
+    """drop the specified columns from the specified dataframes"""
+    for df in dfs:
+        df.drop(cols, axis=1, inplace=True)
+    
+    return dfs
+
+def get_feature_weights(columns:list, model):
+    """Get a dictionary of the features (key) and their weights (value) from a logistic regression.
+    
+    Model should be an sklearn (fitted) logistic regression object
+    
+    Columns should be a list of column names matching the indices of the fitted data."""
+
+
+    assert len(columns) == len(model.coef_.squeeze())
+    return dict(zip(columns, model.coef_.squeeze()))
