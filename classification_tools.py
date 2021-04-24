a5b7ac8a114061ba01b1b62e3cb164fc0ad6940b
Adam Hardy <adh167@gmail.com>
2021-04-24 10:42:05 +0100

    refactored/renamed BinaryLogisticClassifier

--- classification_tools.py
+++ classification_tools.py
@@ -1,10 +1,9 @@
 import plotly.express as px
 import plotly.graph_objects as go
-from sklearn.metrics import roc_curve
-from sklearn.metrics import roc_auc_score
+from sklearn.metrics import roc_curve, confusion_matrix, roc_auc_score
 import numpy as np
-from sklearn.metrics import confusion_matrix
 from matplotlib import pyplot as plt
+from sklearn.linear_model import LogisticRegression
 
 def drop_multi_cols(dfs:list, cols:list):
     """drop the specified columns from the specified dataframes"""
@@ -21,92 +20,155 @@ def predict_with_threshold(model, data, threshold:float=0.5):
 
 
 def list_onehot_columns(df, column_prefix):
+    """returns a list of columns from a dataframe that begin wih the given column_prefix"""
     return df.columns[df.columns.str.contains(f"{column_prefix}.*", regex=True)].to_list()
 
 
-class ModelAnalysis():
+class BinaryLogisticClassifier():
 
-    def __init__(self, model, X_train, X_val, y_train, y_val, col_names):
+    def __init__(self, X_train, X_val, y_train, y_val, col_names, max_iter=100, n_jobs=1):
         self.X_train = X_train
         self.X_val = X_val
         self.y_train = y_train
         self.y_val = y_val
         self.col_names = col_names
-        self.model = model
 
-    def analyse(self):
-        self.accuracy_train = self.model.score(self.X_train, self.y_train)
-        self.accuracy_val = self.model.score(self.X_train, self.y_train)
+        self.model = LogisticRegression(
+            penalty="none",
+            max_iter=max_iter,
+            n_jobs=n_jobs
+            )
+        self.fit()
+
+    def fit(self):
+       self.model.fit(self.X_train, self.y_train)
+
+    def predict(self, threshold=0.5):
+        self.y_train_pred = predict_with_threshold(self.model, self.X_train, threshold=threshold)
+        self.y_val_pred = predict_with_threshold(self.model, self.X_val, threshold=threshold)
+
+    def analyse(self, threshold=0.5):
+
+        self.get_accuracy()
         print("Training accuracy: {:.3f}\nValidation accuracy:{:.3f}".format(self.accuracy_train, self.accuracy_val))
 
-        self.roc_train, self.auc_train = self.calc_roc(self.X_train, self.y_train, title="Training ROC Curve")
-        self.roc_train.show()
-        print("Train AUC ROC score: {:.3f}".format(self.auc_train))
-        self.roc_val, self.auc_val = self.calc_roc(self.X_val, self.y_val, title="Validation ROC Curve")
-        self.roc_val.show()
-        print("AUC ROC score: {:.3f}".format(self.auc_val))
+        self.plot_roc()
 
-        self.y_train_pred = predict_with_threshold(self.model, self.X_train, threshold=0.5)
-        self.conf_mat = confusion_matrix(self.y_train, self.y_train_pred)
-        self.tn, self.fp, self.fn, self.tp = self.conf_mat.ravel()
+        self.predict(threshold=threshold)
 
-        print("True Negatives: {:,}\nFalse Positives: {:,}\nFalse Negatives: {:,}\nTrue Positives: {:,}\n".format(self.tn, self.fp, self.fn, self.tp))
-
-        self.tpr = self.tp/(self.tp+self.fn)
-        self.fpr = self.fp/(self.tn+self.fp)
-
-        print("True positive rate (selectivity): {:3f}".format(self.tpr))
-        print("False positive rate (fall-out): {:3f}".format(self.fpr))
-
+        self.get_confusion_matrix()
         self.plot_confusion_matrix()
+        
+        self.get_tpr_fpr()
+        print("Training:")
+        print("True positive rate (selectivity): {:3f}".format(self.tpr_train))
+        print("False positive rate (fall-out): {:3f}".format(self.fpr_train))
+        print("")
+        print("Validation:")
+        print("True positive rate (selectivity): {:3f}".format(self.tpr_train))
+        print("False positive rate (fall-out): {:3f}".format(self.fpr_train))
 
-        print("Coefficents:")
-        self.coefficients = get_feature_weights(self.col_names, self.model)
+    def get_tpr_fpr(self):
+        self.tpr_train = self.tp_train/(self.tp_train + self.fn_train)
+        self.fpr_train = self.fp_train/(self.fp_train + self.tn_train)
+        self.tpr_val = self.tp_val/(self.tp_val + self.fn_val)
+        self.fpr_val = self.fp_val/(self.fp_val + self.tn_val)
+
+    def get_accuracy(self):
+        self.accuracy_train = self.model.score(self.X_train, self.y_train)
+        self.accuracy_val = self.model.score(self.X_train, self.y_train)
+
+    def get_confusion_matrix(self):
+
+        self.conf_mat_train = confusion_matrix(self.y_train, self.y_train_pred)
+        self.tn_train, self.fp_train, self.fn_train, self.tp_train = self.conf_mat_train.ravel()
+        self.conf_mat_val = confusion_matrix(self.y_val, self.y_val_pred)
+        self.tn_val, self.fp_val, self.fn_val, self.tp_val = self.conf_mat_val.ravel()
 
     def plot_confusion_matrix(self):
     
-        # conf_mat = confusion_matrix(y_true=labels, y_pred=predictions)
-        print('Confusion matrix:\n', self.conf_mat)
+        cm = self.conf_mat_train
 
-        ax_labels = ['Class 0', 'Class 1']
-        fig = plt.figure()
-        ax = fig.add_subplot(111)
-        cax = ax.matshow(self.conf_mat, cmap=plt.cm.Blues)
-        fig.colorbar(cax)
-        ax.set_xticklabels([''] + ax_labels)
-        ax.set_yticklabels([''] + ax_labels)
-        plt.title("Confusion Matrix")
+        plt.clf()
+        plt.imshow(cm, interpolation='nearest', cmap=plt.cm.Wistia)
+        classNames = ['Negative','Positive']
+        plt.title('Confusion Matrix - Test Data')
+        plt.ylabel('Truth')
         plt.xlabel('Predicted')
-        plt.ylabel('Expected')
-        plt.show()
+        tick_marks = np.arange(len(classNames))
+        plt.xticks(tick_marks, classNames, rotation=45)
+        plt.yticks(tick_marks, classNames)
+        s = [['TN','FP'], ['FN', 'TP']]
+        for i in range(2):
+            for j in range(2):
+                plt.text(j-0.25,i, str(s[i][j])+" = "+str(cm[i][j]))
+        self.conf_mat_train_plt = plt
+        self.conf_mat_train_plt.show()
 
-    def calc_roc(self, data, labels, size=800, title="ROC Curve"):
-        """Plots the receiver operating characteristics curve"""
 
-        fpr, tpr, threshold = roc_curve(labels, self.model.predict_proba(data)[:,1])
+        cm = self.conf_mat_val
+
+        plt.clf()
+        plt.imshow(cm, interpolation='nearest', cmap=plt.cm.Wistia)
+        classNames = ['Negative','Positive']
+        plt.title('Confusion Matrix - Validation Data')
+        plt.ylabel('Truth')
+        plt.xlabel('Predicted')
+        tick_marks = np.arange(len(classNames))
+        plt.xticks(tick_marks, classNames, rotation=45)
+        plt.yticks(tick_marks, classNames)
+        s = [['TN','FP'], ['FN', 'TP']]
+        for i in range(2):
+            for j in range(2):
+                plt.text(j-0.25,i, str(s[i][j])+" = "+str(cm[i][j]))
+        self.conf_mat_val_plt = plt
+        self.conf_mat_val_plt.show()
+
+    def plot_roc(self, size=800, title_prefix=''):
 
         width = size
         height = width
 
-        roc = px.line(x=fpr, y=tpr, width=width, height=height)
-        baseline = px.line(x=[0,1], y =[0,1], width=width, height=height)
-        baseline.update_traces(line=dict(color = 'rgba(50,50,50,0.2)'))
+        fpr_train, tpr_train, threshold_train = roc_curve(self.y_train, self.model.predict_proba(self.X_train)[:,1])
+        fpr_val, tpr_val, threshold_val = roc_curve(self.y_val, self.model.predict_proba(self.X_val)[:,1])
 
-        fig = go.Figure(data=roc.data + baseline.data)
-        fig.update_layout(
+        self.roc_curve = go.Figure()
+        self.roc_curve.update_layout(
             autosize=False,
             width=width,
             height=height,
             xaxis_title="False Positive Rate (Sensitivty)",
             yaxis_title="True Positive Rate (1-Precision)",
-            title=title,
+            title=f"{title_prefix} ROC Curve",
             yaxis_range=[0,1],
-            xaxis_range=[0,1]
+            xaxis_range=[0,1],
+            legend=dict(
+                x=0.78,
+                y=0.1,
+                title_text='Data'
+                )
             )
 
-        auc_roc = roc_auc_score(labels, self.model.predict_proba(data)[:,1])
+        self.roc_curve.add_trace(go.Scatter(x=fpr_train, y=tpr_train, name="Training", mode="lines"))
+        self.roc_curve.add_trace(go.Scatter(x=fpr_val, y=tpr_val, name="Validation", mode="lines"))
+        self.roc_curve.add_trace(go.Scatter(x=[0,1], y =[0,1], name="Basline", mode="lines", line=dict(color = 'rgba(50,50,50,0.2)')))
 
-        return fig, auc_roc
+        self.get_auc_roc()
+
+        self.roc_curve.add_annotation(
+            x=0.2,y=0.9,
+            text="AUC ROC score:</br></br>   Training: {:.3f}</br>   Validation: {:.3f}".format(self.auc_roc_train, self.auc_roc_val),
+            align="left",
+            showarrow=False,
+            )
+
+        self.roc_curve.show()
+  
+    def get_auc_roc(self):
+        self.auc_roc_train = roc_auc_score(self.y_train, self.model.predict_proba(self.X_train)[:,1])
+        self.auc_roc_val = roc_auc_score(self.y_val, self.model.predict_proba(self.X_val)[:,1])
+
+        return self.auc_roc_train, self.auc_roc_val
 
     def get_feature_weights(self, print_coef=True):
         """Get a dictionary of the features (key) and their weights (value) from a logistic regression, sorted by absolute value.
@@ -116,7 +178,6 @@ class ModelAnalysis():
         Columns should be a list of column names matching the indices of the fitted data.
         
         TODO: Doesn't work on single feature models."""
-
 
         assert len(self.col_names) == len(self.model.coef_.squeeze())
 
@@ -129,5 +190,6 @@ class ModelAnalysis():
             coefficients_sorted.sort(reverse=True)
 
             # print from largest to smallest
+            print("Coefficents:")
             for abs_val, val, key in coefficients_sorted:
                 print("{: .2e}: {}".format(val, key))
